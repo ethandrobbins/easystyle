b87d54078f677e1da5567115a42e677fa58ffddf
ethandrobbins <52843600+ethandrobbins@users.noreply.github.com>
2019-08-28 20:45:15 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
-  name = 'easystyle',         # How you named your package folder (MyLib)
-  packages = ['easystyle'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A library that allows the user to quickly analyze data at a basic level without exporting to excel',   # Give a short description about your library
-  author = 'Ethan Robbins',                   # Type in your name
-  author_email = 'ethan.d.robbins@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ethandrobbins/easystyle',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ethandrobbins/easystyle/archive/v_1_0.tar.gz',    # I explain this later on
-  keywords = ['STYLE', 'PANDAS', 'DATA'],   # Keywords that define your package best
-  install_requires=[
-          'numpy',
-          'pandas',
-          'seaborn',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7',
-      ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easystyle",
+    version="1.2.1",
+    author="Ethan Robbins",
+    author_email="ethan.d.robbins@gmail.com",
+    description="A package to allow for basic visuals with pandas dataframes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ethandrobbins/easystyle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
